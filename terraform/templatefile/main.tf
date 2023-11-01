2ac13c5d1b71696603d4b819ef0c2ebe0a9e4208
Brendon Thiede <brendonthiede@filevine.com>
2023-10-31 21:01:39 -0400

    Updating TF example

--- terraform/templatefile/main.tf
+++ terraform/templatefile/main.tf
@@ -6,16 +6,19 @@ locals {
   generic_userdata = templatefile("${path.module}/userdata.tftpl", {
     is_k8s        = []
     instance_role = ""
+    special_message = "This is generic"
   })
 
   k8s_userdata = templatefile("${path.module}/userdata.tftpl", {
     is_k8s        = ["true"]
     instance_role = "k8s"
+    special_message = "This is k8s"
   })
 
   special_userdata = templatefile("${path.module}/userdata.tftpl", {
-    is_k8s        = []
-    instance_role = "special"
+    is_k8s          = []
+    instance_role   = "special"
+    special_message = "This is a special instance"
   })
 }
 
@@ -33,4 +36,11 @@ output "special_userdata" {
 
 output "token" {
   value = "asdfasdf"
+}
+
+output "object_example" {
+  value = {
+    "one" = "two"
+    "three" = "four"
+  }
 }
